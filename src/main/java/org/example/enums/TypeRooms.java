2c527a9814372314b4c1088c3f1f296721eed459
Xumoyun <102345213+KhaliBaniya@users.noreply.github.com>
2023-03-22 09:22:47 +0500

    Library Enums committed

---
+++ src/main/java/org/example/enums/TypeRooms.java
@@ -0,0 +1,9 @@
+package org.example.enums;
+
+public enum TypeRooms {
+    /*oddiy hona, lixe hona, ikkitalik hona, oilaviy, Prezidentskiy*/
+    simple_room,
+    luxury_room,
+    double_room, family_room,
+    presidential_room
+}
